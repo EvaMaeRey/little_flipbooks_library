cbec70d0576cdf5ab6eeb65b5d81363d11d6ec86
EvaMaeRey <evangeline.mae@gmail.com>
2019-07-31 15:43:33 +0200

    fix plus sign in operator - to work in Regex

--- xaringan_reveal_parentheses_balanced.R
+++ xaringan_reveal_parentheses_balanced.R
@@ -17,7 +17,7 @@ parse_code <- function(code) {
     mutate(code = str_remove(code, "\\s+$")) %>% 
     mutate(connector = str_extract(code, "%>%$|\\+$|->$|%\\+%")) %>% 
     mutate(connector = replace_na(connector, "")) %>% 
-    mutate(code = str_remove(code, "%>%$|\\+$|->|%+%$")) %>% 
+    mutate(code = str_remove(code, "%>%$|\\+$|->|%\\+%$")) %>% 
     mutate(num_open_par = str_count(code, "\\(|\\{|\\[")) %>% # Counting open parentheses
     mutate(num_closed_par = str_count(code, "\\)|\\}|\\]")) %>%  # Counting closed parentheses
     mutate(balanced_par = (cumsum(num_open_par) - cumsum(num_closed_par)) == 0 &
